e4df19e43965dd39b3438b28bdf2a5c826d7e602
@shhhh chouhan <amishachouhanrajput@gmail.com>
2025-09-08 11:11:12 +0530

    Added edit/delete functionality in todos and dark-light theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,20 @@ import './index.css'
     console.log(todos)
   }
 
-  const handleEdit = () =>{
-
+  const handleEdit = (e,id) =>{
+    let t=todos.filter(i=>i.id === id)
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    })
+    setTodos(newTodos)
   }
   const handleDelete = (e, id) => {
     // let index =todos.findIndex(item => {
     //   return item.id===id
     // })
-   
-
     let newTodos = todos.filter(item=>{
-      return item.id !==id
+      return item.id!==id
     })
     setTodos(newTodos)
   }
@@ -60,14 +63,15 @@ import './index.css'
   
   }
   return (
-    <>
+    < >
+    <div className='bg-violet-100 dark:bg-blue-300 min-h-max '>
       <Navbar toggleTheme={toggleTheme} theme={theme} />
 
       
-      <div className="container mx-auto max-w-2xl my-10 rounded-xl p-6 bg-violet-100 dark:bg-gray-900 shadow-lg min-h-[80vh] transition-colors duration-300">
+      <div className="container mx-auto max-w-2xl my-10 rounded-xl p-6  bg-blue-300 dark:bg-indigo-800 shadow-lg min-h-[80vh] transition-colors duration-300">
 
         
-        <h1 className="text-2xl font-bold text-violet-900 dark:text-violet-100 mb-4 text-center">
+        <h1 className="text-2xl font-bold text-violet-900 dark:text-violet-100  mb-4 text-center">
           Add a Todo
         </h1>
         <div className="addTodo flex flex-col sm:flex-row items-center gap-3 mb-6">
@@ -83,17 +87,17 @@ import './index.css'
         </div>
 
         
-        <h2 className="text-xl font-semibold text-gray-700 mb-3">Your Todos</h2>
+        <h2 className="text-xl font-semibold text-gray-700 mb-3 dark:text-white ">Your Todos</h2>
         <div className="todos space-y-3">
-          {todos.length===0 && <p className='text-gray-500'>no todos available please add some todos</p>}
+          {todos.length===0 && <p className='text-gray-500 dark:text-white'>no todos available please add some todos</p>}
             {todos.map(item => {
 
                  
-         return  <div key={item.id} className="todo flex justify-between items-center bg-white dark:bg-gray-700 shadow-md rounded-md p-3">
+         return  <div key={item.id} className="todo flex justify-between items-center bg-white dark:bg-white shadow-md rounded-md p-3">
             <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted}  id='' />
-            <div className= '{item.isCompleted?"": "line-through"} text-gray-800 dark:text-gray-100'>{item.todo}</div>
+            <div className= '{item.isCompleted?"": "line-through"} text-gray-800 dark:black'>{item.todo}</div>
             <div className="flex gap-2">
-              <button onClick={handleEdit} className="bg-yellow-500 hover:bg-yellow-600 px-3 py-1 text-sm font-medium text-white rounded-md transition-all duration-200">
+              <button onClick={(e)=>handleEdit(e,item.id)} className="bg-yellow-500 hover:bg-yellow-600 px-3 py-1 text-sm font-medium text-white rounded-md transition-all duration-200">
                 Edit
               </button>
               <button  onClick={(e)=>handleDelete(e,item.id)} className="bg-red-500 hover:bg-red-600 px-3 py-1 text-sm font-medium text-white rounded-md transition-all duration-200">
@@ -104,6 +108,7 @@ import './index.css'
             })}
         </div>  
       </div>
+    </div>
     </>
   )
 }
